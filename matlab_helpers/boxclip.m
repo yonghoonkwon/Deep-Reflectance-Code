f349a9076d576125c3365f8d4e7521f2d3b8c605
Hang Zhang <zhang.hang@rutgers.edu>
2016-04-07 17:53:45 -0400

    Delete boxclip.m

--- matlab_helpers/boxclip.m
+++
@@ -1,7 +0,0 @@
-function boxes = boxclip(boxes, imageSize)
-% BOXCLIP Clip boxes to a given image size
-%   BOXES = BOXCLIP(BOXES, IMAGESIZE)
-
-boxes = [max(1,boxes(1:2,:)) ; ...
-         min(imageSize(1),boxes(3,:)) ;
-         min(imageSize(2),boxes(4,:))] ;
